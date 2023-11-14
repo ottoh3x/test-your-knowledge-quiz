9618771d11f67bbe5fb88710261db45dab4ec047
Othmane Khess <otmankhess@gmail.com>
2023-11-14 18:06:32 +0100

    first

---
+++ utils/qsts.ts
@@ -0,0 +1,321 @@
+export const questions = [
+    {
+      question: "What is the capital of Australia?",
+      correctAnswer: "Canberra",
+      choices: ["Sydney", "Melbourne", "Brisbane", "Canberra"]
+    },
+    {
+      question: "Who is known as the 'Father of Computer Science'?",
+      correctAnswer: "Alan Turing",
+      choices: ["Bill Gates", "Ada Lovelace", "Alan Turing", "Charles Babbage"]
+    },
+    {
+      question: "What is the largest mammal in the world?",
+      correctAnswer: "Blue Whale",
+      choices: ["Elephant", "Giraffe", "Blue Whale", "Hippopotamus"]
+    },
+    {
+      question: "Which planet is known as the 'Red Planet'?",
+      correctAnswer: "Mars",
+      choices: ["Venus", "Jupiter", "Mars", "Saturn"]
+    },
+    {
+      question: "Who painted the Mona Lisa?",
+      correctAnswer: "Leonardo da Vinci",
+      choices: ["Vincent van Gogh", "Pablo Picasso", "Leonardo da Vinci", "Michelangelo"]
+    },
+    {
+      question: "What is the currency of Japan?",
+      correctAnswer: "Yen",
+      choices: ["Won", "Dollar", "Euro", "Yen"]
+    },
+    {
+      question: "Which famous scientist developed the theory of general relativity?",
+      correctAnswer: "Albert Einstein",
+      choices: ["Isaac Newton", "Galileo Galilei", "Marie Curie", "Albert Einstein"]
+    },
+    {
+      question: "In which year did the Berlin Wall fall?",
+      correctAnswer: "1989",
+      choices: ["1975", "1989", "1995", "2001"]
+    },
+    {
+      question: "What is the chemical symbol for water?",
+      correctAnswer: "H2O",
+      choices: ["CO2", "O2", "H2O", "N2"]
+    },
+    {
+      question: "Who wrote 'To Kill a Mockingbird'?",
+      correctAnswer: "Harper Lee",
+      choices: ["J.K. Rowling", "Ernest Hemingway", "Harper Lee", "George Orwell"]
+    },
+    {
+      question: "What is the capital of Canada?",
+      correctAnswer: "Ottawa",
+      choices: ["Toronto", "Vancouver", "Montreal", "Ottawa"]
+    },
+    {
+      question: "Who discovered penicillin?",
+      correctAnswer: "Alexander Fleming",
+      choices: ["Marie Curie", "Louis Pasteur", "Alexander Fleming", "Joseph Lister"]
+    },
+    {
+      question: "Which gas is most abundant in the Earth's atmosphere?",
+      correctAnswer: "Nitrogen",
+      choices: ["Oxygen", "Carbon Dioxide", "Argon", "Nitrogen"]
+    },
+    {
+      question: "What is the largest ocean on Earth?",
+      correctAnswer: "Pacific Ocean",
+      choices: ["Atlantic Ocean", "Indian Ocean", "Southern Ocean", "Pacific Ocean"]
+    },
+    {
+      question: "Who is the author of 'The Great Gatsby'?",
+      correctAnswer: "F. Scott Fitzgerald",
+      choices: ["Ernest Hemingway", "Jane Austen", "F. Scott Fitzgerald", "Mark Twain"]
+    },
+    {
+      question: "What is the main component of the Earth's core?",
+      correctAnswer: "Iron",
+      choices: ["Gold", "Nickel", "Copper", "Iron"]
+    },
+    {
+      question: "Which element has the atomic number 1?",
+      correctAnswer: "Hydrogen",
+      choices: ["Oxygen", "Helium", "Hydrogen", "Carbon"]
+    },
+    {
+      question: "When did World War II end?",
+      correctAnswer: "1945",
+      choices: ["1939", "1945", "1950", "1960"]
+    },
+    {
+      question: "What is the chemical symbol for sodium?",
+      correctAnswer: "Na",
+      choices: ["K", "Ca", "Na", "Mg"]
+    },
+    {
+      question: "Who wrote 'Pride and Prejudice'?",
+      correctAnswer: "Jane Austen",
+      choices: ["Charlotte Brontë", "Emily Brontë", "Jane Austen", "Charles Dickens"]
+    },
+    {
+      question: "What is the capital of South Africa?",
+      correctAnswer: "Pretoria",
+      choices: ["Cape Town", "Johannesburg", "Durban", "Pretoria"]
+    },
+    {
+      question: "Who discovered the theory of evolution by natural selection?",
+      correctAnswer: "Charles Darwin",
+      choices: ["Gregor Mendel", "Alfred Russel Wallace", "Charles Lyell", "Charles Darwin"]
+    },
+    {
+      question: "What is the smallest prime number?",
+      correctAnswer: "2",
+      choices: ["1", "2", "3", "5"]
+    },
+    {
+      question: "Which planet is known as the 'Morning Star' or 'Evening Star'?",
+      correctAnswer: "Venus",
+      choices: ["Mercury", "Venus", "Mars", "Jupiter"]
+    },
+    {
+      question: "Who wrote 'The Catcher in the Rye'?",
+      correctAnswer: "J.D. Salinger",
+      choices: ["F. Scott Fitzgerald", "J.D. Salinger", "Ernest Hemingway", "Harper Lee"]
+    },
+    {
+      question: "What is the largest desert in the world?",
+      correctAnswer: "Antarctica",
+      choices: ["Sahara", "Gobi", "Antarctica", "Arabian"]
+    },
+    {
+      question: "Who painted 'Starry Night'?",
+      correctAnswer: "Vincent van Gogh",
+      choices: ["Pablo Picasso", "Claude Monet", "Vincent van Gogh", "Leonardo da Vinci"]
+    },
+    {
+      question: "What is the capital of Brazil?",
+      correctAnswer: "Brasília",
+      choices: ["Rio de Janeiro", "São Paulo", "Brasília", "Salvador"]
+    },
+    {
+      question: "Which country is known as the 'Land of the Rising Sun'?",
+      correctAnswer: "Japan",
+      choices: ["China", "South Korea", "Japan", "Vietnam"]
+    },
+    {
+      question: "Who wrote 'The Odyssey'?",
+      correctAnswer: "Homer",
+      choices: ["Virgil", "Homer", "Sophocles", "Aristotle"]
+    },
+    {
+      question: "What is the capital of Russia?",
+      correctAnswer: "Moscow",
+      choices: ["St. Petersburg", "Kiev", "Moscow", "Minsk"]
+    },
+    {
+      question: "Who developed the theory of relativity alongside Albert Einstein?",
+      correctAnswer: "Mileva Marić",
+      choices: ["Niels Bohr", "Mileva Marić", "Werner Heisenberg", "Max Planck"]
+    },
+    {
+      question: "What is the speed of light?",
+      correctAnswer: "299,792 kilometers per second",
+      choices: ["150,000 kilometers per second", "299,792 kilometers per second", "500,000 kilometers per second", "200,000 kilometers per second"]
+    },
+    {
+      question: "Who is the author of 'The Hobbit'?",
+      correctAnswer: "J.R.R. Tolkien",
+      choices: ["C.S. Lewis", "J.K. Rowling", "J.R.R. Tolkien", "George R.R. Martin"]
+    },
+    {
+      question: "What is the largest continent?",
+      correctAnswer: "Asia",
+      choices: ["Africa", "Europe", "Asia", "North America"]
+    },
+    {
+      question: "Who discovered the double helix structure of DNA?",
+      correctAnswer: "James Watson and Francis Crick",
+      choices: ["Rosalind Franklin", "James Watson and Francis Crick", "Linus Pauling", "Gregor Mendel"]
+    },
+    {
+      question: "Which element has the chemical symbol 'Fe'?",
+      correctAnswer: "Iron",
+      choices: ["Gold", "Silver", "Iron", "Copper"]
+    },
+    {
+      question: "What is the largest species of big cat?",
+      correctAnswer: "Siberian Tiger",
+      choices: ["Lion", "Jaguar", "Leopard", "Siberian Tiger"]
+    },
+    {
+      question: "In which year did the United States declare independence?",
+      correctAnswer: "1776",
+      choices: ["1600", "1776", "1865", "1945"]
+    },
+    {
+      question: "What is the capital of China?",
+      correctAnswer: "Beijing",
+      choices: ["Shanghai", "Hong Kong", "Beijing", "Guangzhou"]
+    },
+    {
+      question: "Who is known as the 'Queen of Pop'?",
+      correctAnswer: "Madonna",
+      choices: ["Beyoncé", "Madonna", "Lady Gaga", "Rihanna"]
+    },
+    {
+      question: "What is the square root of 144?",
+      correctAnswer: "12",
+      choices: ["8", "10", "12", "15"]
+    },
+    {
+      question: "Who was the first woman to win a Nobel Prize?",
+      correctAnswer: "Marie Curie",
+      choices: ["Rosalind Franklin", "Marie Curie", "Dorothy Crowfoot Hodgkin", "Gerty Cori"]
+    },
+    {
+      question: "What is the capital of South Korea?",
+      correctAnswer: "Seoul",
+      choices: ["Busan", "Incheon", "Seoul", "Daegu"]
+    },
+    {
+      question: "Who wrote 'War and Peace'?",
+      correctAnswer: "Leo Tolstoy",
+      choices: ["Fyodor Dostoevsky", "Leo Tolstoy", "Anton Chekhov", "Nikolai Gogol"]
+    },
+    {
+      question: "What is the chemical symbol for gold?",
+      correctAnswer: "Au",
+      choices: ["Ag", "Fe", "Cu", "Au"]
+    },
+    {
+      question: "Who composed the 'Moonlight Sonata'?",
+      correctAnswer: "Ludwig van Beethoven",
+      choices: ["Wolfgang Amadeus Mozart", "Ludwig van Beethoven", "Johann Sebastian Bach", "Frederic Chopin"]
+    },
+    {
+      question: "What is the largest planet in our solar system?",
+      correctAnswer: "Jupiter",
+      choices: ["Mars", "Saturn", "Earth", "Jupiter"]
+    },
+    {
+      question: "Who is the author of 'One Hundred Years of Solitude'?",
+      correctAnswer: "Gabriel García Márquez",
+      choices: ["Isabel Allende", "Gabriel García Márquez", "Mario Vargas Llosa", "Julio Cortázar"]
+    },
+    {
+      question: "What is the capital of France?",
+      correctAnswer: "Paris",
+      choices: ["Berlin", "Madrid", "Rome", "Paris"]
+    },
+    {
+      question: "Who discovered electricity?",
+      correctAnswer: "Benjamin Franklin",
+      choices: ["Thomas Edison", "Nikola Tesla", "Benjamin Franklin", "Alexander Graham Bell"]
+    },
+    {
+      question: "What is the chemical symbol for oxygen?",
+      correctAnswer: "O2",
+      choices: ["N2", "CO2", "O2", "H2"]
+    },
+    {
+      question: "Which planet is known as the 'Blue Planet'?",
+      correctAnswer: "Earth",
+      choices: ["Venus", "Mars", "Earth", "Neptune"]
+    },
+    {
+      question: "Who is the author of 'The Canterbury Tales'?",
+      correctAnswer: "Geoffrey Chaucer",
+      choices: ["William Shakespeare", "John Milton", "Geoffrey Chaucer", "Jane Austen"]
+    },
+    {
+      question: "What is the capital of Italy?",
+      correctAnswer: "Rome",
+      choices: ["Milan", "Venice", "Florence", "Rome"]
+    },
+    {
+      question: "Who is the Greek god of the sea?",
+      correctAnswer: "Poseidon",
+      choices: ["Zeus", "Hades", "Athena", "Poseidon"]
+    },
+    {
+      question: "What is the chemical symbol for carbon?",
+      correctAnswer: "C",
+      choices: ["Ca", "Co", "C", "Cu"]
+    },
+    {
+      question: "Who painted 'The Persistence of Memory'?",
+      correctAnswer: "Salvador Dalí",
+      choices: ["Pablo Picasso", "Vincent van Gogh", "Salvador Dalí", "Claude Monet"]
+    },
+    {
+      question: "What is the capital of Egypt?",
+      correctAnswer: "Cairo",
+      choices: ["Alexandria", "Luxor", "Cairo", "Giza"]
+    },
+    {
+      question: "Who wrote 'Frankenstein'?",
+      correctAnswer: "Mary Shelley",
+      choices: ["Emily Brontë", "Jane Austen", "Mary Shelley", "Charlotte Brontë"]
+    },
+    {
+      question: "What is the square root of 169?",
+      correctAnswer: "13",
+      choices: ["10", "12", "13", "15"]
+    },
+    {
+      question: "Who is the author of 'The Picture of Dorian Gray'?",
+      correctAnswer: "Oscar Wilde",
+      choices: ["James Joyce", "Oscar Wilde", "Virginia Woolf", "D.H. Lawrence"]
+    },
+    {
+      question: "What is the largest mountain on Earth?",
+      correctAnswer: "Mount Everest",
+      choices: ["K2", "Kangchenjunga", "Lhotse", "Mount Everest"]
+    },
+    {
+      question: "Who discovered the laws of motion and gravity?",
+      correctAnswer: "Isaac Newton",
+      choices: ["Galileo Galilei", "Johannes Kepler", "Isaac Newton", "Otto Programmer"] }
+    ]  
